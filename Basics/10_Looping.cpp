2c2fd99fab26afc3784c1cdeeb1b02212524dffc
sakshimali0307 <127278584+sakshimali0307@users.noreply.github.com>
2023-11-14 21:33:47 +0530

    Basics

---
+++ Basics/10_Looping.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+ int main()
+ {
+  int i = 0;
+
+  while(i < 10)
+  {
+   cout<<"\nWelcome\n",i+101;
+   i++;
+  }
+  cout<<"\nThanks!!!...";
+
+  getch();
+  return 0;
+ }
