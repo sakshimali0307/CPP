069127404244b04d0467d73a67149261c74a6273
sakshimali0307 <127278584+sakshimali0307@users.noreply.github.com>
2023-11-25 20:58:17 +0530

    Pattern

---
+++ Basics/22_Pattern  For Right Lower Triangular.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int i = 0, j = 0, x = 0;
+
+    cout<<"\n Enter Size For Pattern :";
+    cin>>x;
+
+    cout<<"\n\n *********** Pattern ********** \n\n";
+
+    for(i = 1; i <= x; i++)
+    {
+        for(j = 1; j <= x; j++)
+        {
+            if(i + j <= x + 1)
+            {
+                cout<<" * ";
+            }
+            else
+            {
+                cout<<"   ";
+            }
+        }
+        cout<<"\n";
+    }
+    cout<<"===================================0";
+    getch();
+    return 0;
+}
