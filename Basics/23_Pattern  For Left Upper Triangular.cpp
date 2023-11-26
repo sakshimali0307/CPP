575e7e61209474fb55ef1cf707fe5fbd5aeed539
sakshimali0307 <127278584+sakshimali0307@users.noreply.github.com>
2023-11-26 20:18:07 +0530

    Pattern

---
+++ Basics/23_Pattern  For Left Upper Triangular.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int i = 0, j = 0, x = 0;
+
+    cout<<"\n Enter Size For Pattern :";
+    cin>>x;
+
+    cout<<"\n\n********** Pattern **********\n\n";
+
+    for(i = 1; i <= x; i++)
+    {
+        for(j = 1; j <= x; j++)
+        {
+            if(i+j <= x+1)
+            {
+                cout<<" * ";
+            }
+            else
+            {
+                cout<<"   ";
+            }
+        }
+        cout<<"\n";
+    }
+    cout<<"\n============================\n";
+
+    getch();
+    return 0;
+}
