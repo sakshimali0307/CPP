e18568e80a79356ec356c4a41273e25e2b50e447
sakshimali0307 <127278584+sakshimali0307@users.noreply.github.com>
2023-12-04 21:34:15 +0530

    Basics

---
+++ Basics/31_Array.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+ char Arr[5] = {'F', '%','9', '#','X'};
+
+ cout<<"\nValue of 1 Element ="<<Arr[0];
+ cout<<"\nValue of 2 Element ="<<Arr[1];
+ cout<<"\nValue of 3 Element ="<<Arr[2];
+ cout<<"\nValue of 4 Element ="<<Arr[3];
+ cout<<"\nValue of 5 Element ="<<Arr[4];
+
+ getch();
+ return 0;
+ }
