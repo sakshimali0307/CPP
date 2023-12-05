608c3fe8443d9aa96a2f469986b00529cfdc8111
sakshimali0307 <127278584+sakshimali0307@users.noreply.github.com>
2023-12-05 21:50:34 +0530

    Basics

---
+++ Basics/32_Array.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+ int values[6] = {12, 232, 45, 56, 78, 65};
+
+ cout<<"\n Values of 101 Element ="<<values[0];
+ cout<<"\n Values of 102 Element ="<<values[1];
+ cout<<"\n Values of 103 Element ="<<values[2];
+ cout<<"\n Values of 104 Element ="<<values[3];
+ cout<<"\n Values of 105 Element ="<<values[4];
+ cout<<"\n Values of 106 Element ="<<values[5];
+
+ getch();
+ return 0;
+}
