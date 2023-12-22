4d66d6303926135ad78e8728573b4dde896b4374
sakshimali0307 <127278584+sakshimali0307@users.noreply.github.com>
2023-12-22 21:30:42 +0530

    Basics

---
+++ Basics/45_Array with Null Initilazation.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+ int value[7] = {};
+
+ cout<<"\nvalue of 1st element = "<<value[0];
+ cout<<"\nvalue of 2nd element = "<<value[1];
+ cout<<"\nvalue of 3rd element = "<<value[2];
+ cout<<"\nvalue of 4th element = "<<value[3];
+ cout<<"\nvalue of 5th element = "<<value[4];
+ cout<<"\nvalue of 6th element = "<<value[5];
+ cout<<"\nvalue of 7th element = "<<value[6];
+
+ cout<<"\n\nthanks.....";
+
+ getch();
+ return 0;
+}
