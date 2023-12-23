9d2af73ed24286df42f6fd28fea0fb5f028067e7
sakshimali0307 <127278584+sakshimali0307@users.noreply.github.com>
2023-12-23 22:14:58 +0530

    Basics

---
+++ Basics/46_Array of 7 elements with null initilazation &without loop display all elements.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+  int value[7]={};
+
+  cout<<"\nValue of 1st Element = "<<value[0];
+  cout<<"\nValue of 2nd Element = "<<value[1];
+  cout<<"\nValue of 3rd Element = "<<value[2];
+  cout<<"\nValue of 4th Element = "<<value[3];
+  cout<<"\nValue of 5th Element = "<<value[4];
+  cout<<"\nValue of 6th Element = "<<value[5];
+  cout<<"\nValue of 7th Element = "<<value[6];
+
+  getch();
+  return 0;
+}
